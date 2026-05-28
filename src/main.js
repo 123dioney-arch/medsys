fd65eb3104a702d352366177efc7f85d8ecaf627
123dioney-arch <123dioney@gmail.com>
2026-05-28 17:15:18 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 window.addEventListener("DOMContentLoaded", async () => {
   try {
-    const { checkUpdate, installUpdate } = window.__TAURI__.updater;
-    const { relaunch } = window.__TAURI__.process;
-    
-    const { shouldUpdate, manifest } = await checkUpdate();
-    if (shouldUpdate) {
-      const yes = confirm(`Nueva versión: ${manifest.version}\n¿Actualizar ahora?`);
-      if (yes) {
-        await installUpdate();
-        await relaunch();
-      }
-    }
+    const updater = window.__TAURI__.updater;
+    console.log("Updater:", JSON.stringify(Object.keys(updater)));
+    alert("Funciones disponibles: " + Object.keys(updater).join(", "));
   } catch (error) {
     alert("Error: " + error.message);
   }
